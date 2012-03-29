a56b96296a0290266be770baa044498bc5b7bff7
Daniel Pavel <daniel.pavel@gmail.com>
2012-03-29 07:55:14 +0300

    print the proper md5 in gen_pw

--- tools/gen_pw.py
+++ tools/gen_pw.py
@@ -3,7 +3,7 @@
 import hashlib
 
 serial = input("Enter your device serial number: ").strip()
-serialmd5 = hashlib.md5((serial + '\n').encode('utf-8'))
+serialmd5 = hashlib.md5((serial + '\n').encode('utf-8')).hexdigest()
 print("Serial md5 is", serialmd5)
-pw = 'fiona' + serialmd5.hexdigest()[7:11]
+pw = 'fiona' + serialmd5[7:11]
 print("Password for device [%s] is [%s]" % (serial, pw))
